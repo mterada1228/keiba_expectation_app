35cb4534879bda76db2dba9e6a64832073ac612a
寺田優 <mterada1228@gmail.com>
2020-09-20 15:20:36 +0900

    競走馬ページに ave1F-RPCI グラフを表示

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -5,24 +5,6 @@ class HorsesController < ApplicationController
 
   def show
     @horse = Horse.find(params[:id])
-    @ploted_data = createPlotedData(@horse.horse_race_results)
-  end
-
-  private
-
-  def createPlotedData(hose_race_results)
-    ploted_data = []
-    racesWin(hose_race_results).each do |hose_race_result|
-      race_result = hose_race_result.race_result
-      ploted_data.push({name: race_result.name, 
-                        data: [[race_result.RPCI, race_result.ave_1F]],
-                        color: "#b00"
-      })
-    end
-    ploted_data
-  end
-
-  def racesWin(hose_race_results)
-    #TODO 優勝データのみ抽出する
+    @data_to_be_plotted = @horse.plot_data
   end
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,4 +1,58 @@
+#
 class Horse < ApplicationRecord
-    has_many :horse_race_results
-    has_many :race_horses
+  has_many :horse_race_results
+  has_many :race_horses
+
+  def plot_data
+    data = []
+    data.concat(races_win)
+    data.concat(races_lost_by_0_point_2_secounds)
+    data.concat(races_lost_by_1_secounds)
+    data.concat(races_lost_by_greater_than_1_secounds)
+  end
+
+  private
+
+  def data_array(races, color)
+    races.map do |race|
+      result = race.race_result
+      { name: result.name,
+        data: [[result.RPCI, result.ave_1F]],
+        color: color }
+    end
+  end
+
+  def races_win
+    races = horse_race_results.select do |horse_race_result|
+      horse_race_result.rank == '1'
+    end
+
+    data_array(races, '#FFFF00')
+  end
+
+  def races_lost_by_0_point_2_secounds
+    races = horse_race_results.select do |horse_race_result|
+      horse_race_result.rank != '1' && horse_race_result.time_diff.to_f <= 0.2
+    end
+
+    data_array(races, '#008000')
+  end
+
+  def races_lost_by_1_secounds
+    races = horse_race_results.select do |horse_race_result|
+      horse_race_result.rank != '1' &&
+      horse_race_result.time_diff.to_f > 0.2 &&
+      horse_race_result.time_diff.to_f <= 1.0
+    end
+
+    data_array(races, '#0000FF')
+  end
+
+  def races_lost_by_greater_than_1_secounds
+    races = horse_race_results.select do |horse_race_result|
+      horse_race_result.rank != '1' && horse_race_result.time_diff.to_f > 1.0
+    end
+
+    data_array(races, '#A9A9A9')
+  end
 end
