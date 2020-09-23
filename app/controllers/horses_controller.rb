81a6d37ab5681d2c9c3cceefce50da135066d309
寺田優 <mterada1228@gmail.com>
2020-09-23 09:33:44 +0900

    move race win method to horse model

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -5,24 +5,6 @@ class HorsesController < ApplicationController
 
   def show
     @horse = Horse.find(params[:id])
-    @ploted_data = createPlotedData(@horse.horse_race_results)
-  end
-
-  private
-
-  def createPlotedData(horse_race_results)
-    ploted_data = []
-    racesWin(horse_race_results).each do |horse_race_result|
-      race_result = horse_race_result.race_result
-      ploted_data.push({name: race_result.name, 
-                        data: [[race_result.RPCI, race_result.ave_1F]],
-                        color: "#ffff00"
-      })
-    end
-    ploted_data
-  end
-
-  def racesWin(horse_race_results)
-    horse_race_results.select { |horse_race_result| horse_race_result.rank == '1' }
+    @data_to_be_plotted = @horse.plot_data
   end
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,4 +1,20 @@
 class Horse < ApplicationRecord
-    has_many :horse_race_results
-    has_many :race_horses
+  has_many :horse_race_results
+  has_many :race_horses
+
+  def plot_data
+    data = []
+    data.concat(races_win)
+  end
+
+  private
+
+  def races_win
+    horse_race_results.map do |horse_race_result|
+      race_result = horse_race_result.race_result
+      { name: race_result.name, 
+        data: [[race_result.RPCI, race_result.ave_1F]],
+        color: "#ffff00" }
+    end
+  end
 end
