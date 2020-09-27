1d02c938f29e02eb33f969bcfa578ef1592d55b3
寺田優 <mterada1228@gmail.com>
2020-09-28 08:58:39 +0900

    add race spec

--- spec/models/race_spec.rb
+++ spec/models/race_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Race, type: :model do
     let(:race_horse3) { FactoryGirl.create(:race_horse, horse_number: '1')}
 
     it '馬番の昇順でソートされていること' do
-      binding.pry
       race_horse = race.race_horses_sorted_by_horse_number
 
       expected(race_horse[0]).to eq(race_horse3)
