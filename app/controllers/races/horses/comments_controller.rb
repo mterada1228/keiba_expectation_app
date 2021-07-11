976943ca6e69962bcd5cabc9c6757de0811ddcc5
mterada1228 <mterada1228@gmail.com>
2021-07-11 14:39:25 +0900

    コメントを投稿し、テーブルに保存する

--- app/controllers/races/horses/comments_controller.rb
+++ app/controllers/races/horses/comments_controller.rb
@@ -2,8 +2,24 @@ module Races
   module Horses
     class CommentsController < ApplicationController
       def index
-        # TODO: 処理を実装
         @position = params[:position]
+        @comment = Comment.new
+      end
+
+      def create
+        @comment = Comment.create(comment_params.merge(race_id: params[:race_id]))
+        if @comment.save
+          flash[:notice] = 'コメントを投稿しました'
+        else
+          flash[:danger] = 'コメントの投稿に失敗しました。'
+        end
+        redirect_to race_horse_comments_path(race_id: params[:race_id], horse_id: params[:horse_id])
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:horse_id, :race_id, :description, :user_name, :position)
       end
     end
   end
