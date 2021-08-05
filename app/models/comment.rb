868963eecb7d8d7f241f70defc9d4aa2762f1d15
mterada1228 <mterada1228@gmail.com>
2021-08-05 09:07:32 +0900

    Comment 保存時に、user name が空文字の場合は nil に変換する

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,12 @@ class Comment < ApplicationRecord
 
   validates :description, presence: true, length: { maximum: 999 }
   validates :user_name, length: { maximum: 29 }
+
+  before_save :convert_user_name_from_blank_to_nil
+
+  private
+
+  def convert_user_name_from_blank_to_nil
+    self.user_name = nil if user_name.blank?
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  describe 'validation' do
-    let(:horse_race) { create(:horse_race) }
+  let(:horse_race) { create(:horse_race) }
 
+  describe 'validation' do
     describe 'description' do
       context '入力されていない場合' do
         let(:comment) { build(:comment, horse_race: horse_race, description: nil) }
@@ -35,6 +35,30 @@ RSpec.describe Comment, type: :model do
           expect(comment).to be_invalid
           expect(comment.errors.full_messages).to include('ニックネームは29文字以内で入力してください')
         end
+      end
+    end
+  end
+
+  describe 'convert_user_name_from_blank_to_nil' do
+    let(:comment) { build(:comment, horse_race: horse_race, user_name: user_name) }
+
+    subject { comment.save }
+
+    context 'user_name が 空文字の場合' do
+      let(:user_name) { '' }
+
+      it 'nil に変換されて保存される' do
+        subject
+        expect(comment.reload.user_name).to be_nil
+      end
+    end
+
+    context 'user_name が 空文字ではない場合' do
+      let(:user_name) { Faker::Name.name }
+
+      it 'nil に変換されず、そのまま保存される' do
+        subject
+        expect(comment.reload.user_name).to eq(user_name)
       end
     end
   end
