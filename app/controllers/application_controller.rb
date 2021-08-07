fb155837516bd1614aa8801595ac1746b78a558f
mterada1228 <mterada1228@gmail.com>
2021-08-07 13:58:07 +0900

    指摘対応: CommentsController#create と CommentsController#set_horse_race の密結合をやめる   - HorseRace は CommentsController#create 内で取得するようにする   - rescue は rescue_from で処理を分離するようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger
+
+  rescue_from ActiveRecord::RecordNotFound do |_e|
+    render template: 'errors/error_404', status: 404
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,15 @@
 class CommentsController < ApplicationController
-  before_action :set_horse_race, only: [:create]
-
   def index
     @position = params[:position]
     @comment = Comment.new
   end
 
   def create
-    @comment = @horse_race.comments.new(comment_params)
+    horse_race = HorseRace.find(params[:horse_race_id]) 
+    @comment = horse_race.comments.new(comment_params)
     if @comment.save
       flash[:success] = 'コメントを投稿しました'
-      redirect_to horse_race_comments_path(@horse_race, params[:position])
+      redirect_to horse_race_comments_path(horse_race, params[:position])
     else
       flash[:danger] = 'コメントの投稿に失敗しました。'
       render 'index'
@@ -21,11 +20,5 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:description, :user_name, :position)
-  end
-
-  def set_horse_race
-    @horse_race = HorseRace.find(params[:horse_race_id])
-  rescue ActiveRecord::RecordNotFound => _e
-    render template: 'errors/error_404', status: 404
   end
 end
