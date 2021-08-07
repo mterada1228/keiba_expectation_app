e639ca30897ad2b431e56449444fed25e8dacda5
mterada1228 <mterada1228@gmail.com>
2021-08-07 14:03:18 +0900

    指摘対応: テスト修正   - Comment の Factory は user_name を 29 文字以内で生成するようにする   - Comment の model スペックは正常系でモデルを create ではなく build で生成する

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
     description { Faker::String.random(length: 999) }
-    user_name { Faker::Name.name }
+    user_name { Faker::Name.initials(number: 29) }
     position { Comment.positions.keys.sample }
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,9 +3,9 @@ describe Comment do
 
   describe 'validation' do
     context '正常系' do
-      let(:comment) { create(:comment, horse_race: horse_race) }
+      let(:comment) { build(:comment, horse_race: horse_race) }
 
-      it '保存に成功する' do
+      it 'バリデーションに成功する' do
         expect(comment).to be_valid
       end
     end
@@ -13,7 +13,7 @@ describe Comment do
     describe 'description' do
       context '入力されていない場合' do
         let(:comment) { build(:comment, horse_race: horse_race, description: nil) }
-        it '保存に失敗する' do
+        it 'バリデーションに失敗する' do
           expect(comment).to be_invalid
           expect(comment.errors.full_messages).to include('コメントを入力してください')
         end
@@ -24,7 +24,7 @@ describe Comment do
           build(:comment, horse_race: horse_race, description: Faker::String.random(length: 1000))
         end
 
-        it '保存に失敗する' do
+        it 'バリデーションに失敗する' do
           expect(comment).to be_invalid
           expect(comment.errors.full_messages).to include('コメントは999文字以内で入力してください')
         end
@@ -37,7 +37,7 @@ describe Comment do
           build(:comment, horse_race: horse_race, user_name: Faker::String.random(length: 30))
         end
 
-        it '保存に失敗する' do
+        it 'バリデーションに失敗する' do
           expect(comment).to be_invalid
           expect(comment.errors.full_messages).to include('ニックネームは29文字以内で入力してください')
         end
