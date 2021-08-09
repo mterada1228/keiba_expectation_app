22709703c77535fc57fe74729f023d059ec43deb
mterada1228 <mterada1228@gmail.com>
2021-08-09 16:21:18 +0900

    Revert "CSS のファイル体系を整理"
    This reverts commit 953aaaeebb3b421aba8a984621f951d8f7f8c04e.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,10 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-
 require("chartkick");
 require("chart.js");
 require("jquery");
 require("bootstrap");
-
-import "bootstrap";
-import "../stylesheets/application";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,3 +18,6 @@ import "../stylesheets/application";
 //
 const images = require.context("../images", true);
 const imagePath = (name) => images(name, true);
+import "../stylesheets/custom.css.scss";
+
+import "bootstrap";
