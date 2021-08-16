ce8c1d8b53598d87c1f7a3fdc27af2661a1fb798
mterada1228 <mterada1228@gmail.com>
2021-08-16 20:34:19 +0900

    指摘対応

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
     description { Faker::String.random(length: 999) }
-    user_name { Faker::Name.initials(number: 29) }
+    user_name { Faker::Name.name[0..29] }
     position { Comment.positions.keys.sample }
   end
 end
