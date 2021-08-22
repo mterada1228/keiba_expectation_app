ca892998b2d980ec96fee2ba41db22010ccf8dbc
mterada1228 <mterada1228@gmail.com>
2021-08-23 08:54:38 +0900

    コメント投稿機能のためのモデリング

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :horse_race, foreign_key: [:horse_id, :race_id]
+  belongs_to :parent, class_name: 'Comment', optional: true
+  has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
 
   enum comment_type: { positive: 0, negative: 1 }
 
